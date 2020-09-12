d5c432298d4b67d221f392cf53f5385243aa7336
forfeen <f.fin07@gmail.com>
2020-09-12 21:05:24 +0700

    Fix some bug

--- kupollsapp/polls/views.py
+++ kupollsapp/polls/views.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 from django.views import generic
 from .models import Question, Choice
 
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
@@ -22,7 +23,7 @@ class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
 
 def vote(request, question_id):
-    question = get_object_or_404(Question, pk = question_id)
+    question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
